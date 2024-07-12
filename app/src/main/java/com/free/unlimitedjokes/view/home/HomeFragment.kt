8b76c4e5ccd7729f55fe7c8f523e9c562700ed81
uthoai <uthoaimarma597@gmail.com>
2024-07-13 02:33:29 +0600

    saveJoke to RoomDatabase

--- app/src/main/java/com/free/unlimitedjokes/view/home/HomeFragment.kt
+++ app/src/main/java/com/free/unlimitedjokes/view/home/HomeFragment.kt
@@ -1,33 +1,55 @@
 package com.free.unlimitedjokes.view.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.free.unlimitedjokes.base.BaseFragment
+import com.free.unlimitedjokes.R
 import com.free.unlimitedjokes.databinding.FragmentHomeBinding
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     private val viewModel by viewModels<HomeViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home,container,false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this.viewLifecycleOwner
 
-    override fun setListener() {
-        val categories = arrayOf("Any", "Programming", "Miscellaneous", "Dark", "Pun", "Spooky", "Christmas")
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, categories)
+        val categoriesList = arrayOf("Any", "Programming", "Miscellaneous", "Dark", "Pun", "Spooky", "Christmas")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, categoriesList)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerCategory.adapter = adapter
 
+        setListener()
+        allObserver()
+
+        return binding.root
+    }
+
+    private fun setListener() {
         binding.btnNext.setOnClickListener {
             val selectedCategory = binding.spinnerCategory.selectedItem.toString()
             viewModel.getRandomJokes(selectedCategory)
         }
-
     }
 
-    override fun allObserver() {
-        viewModel._jokesData.observe(this){data->
-            if (data.joke == null){
-                binding.setup.text = data.setup ?: ""
-                binding.joke.text = data.delivery
-            }else{
-                binding.joke.text = data.joke
+    private fun allObserver() {
+        viewModel._responseJokesData.observe(viewLifecycleOwner){data->
+            data?.let {
+                if (data.joke == null){
+                    viewModel.jokeSetup.postValue(data.setup ?: "")
+                    viewModel.delivery.postValue(data.delivery ?: "")
+                }else{
+                    viewModel.delivery.postValue(data.joke)
+                }
+                viewModel.categories.postValue(data.category ?: "")
             }
         }
     }
--- app/src/main/java/com/free/unlimitedjokes/view/home/HomeViewModel.kt
+++ app/src/main/java/com/free/unlimitedjokes/view/home/HomeViewModel.kt
@@ -1,17 +1,27 @@
 package com.free.unlimitedjokes.view.home
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.free.unlimitedjokes.db.DefaultJokesRepository
+import com.free.unlimitedjokes.db.JokesData
 import com.free.unlimitedjokes.service.JokesClient
 import com.free.unlimitedjokes.service.ResponseJokes
 import kotlinx.coroutines.launch
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
-    private val jokesData = MutableLiveData<ResponseJokes>()
-    val _jokesData get() = jokesData
+    private val responseJokesData = MutableLiveData<ResponseJokes>()
+    val _responseJokesData get() = responseJokesData
+
+    private val emptyValue = ""
+    val jokeSetup = MutableLiveData<String>()
+    val delivery = MutableLiveData<String>()
+    val categories = MutableLiveData<String>()
+    private val repository: DefaultJokesRepository = DefaultJokesRepository.getInstance(application)
+    private lateinit var jokesData: JokesData
 
     fun getRandomJokes(category: String) {
         viewModelScope.launch {
@@ -26,31 +36,38 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
                 else -> JokesClient.jokeApiService.getRandomJoke()
             }
             if (response.isSuccessful) {
-                jokesData.postValue(response.body())
+                responseJokesData.postValue(response.body())
             }
         }
+
     }
 
-    /*fun saveTask() {
-        val currentTitle = title.value
-        val currentDescription = description.value
-        val task = Task(
-            id = currentTaskID,
-            title = currentTitle.toTrimString(),
-            description = currentDescription.toTrimString()
-        )
+    fun saveJokes() {
+        val jokeSetup = jokeSetup.value
+        val delivery = delivery.value
 
-        if (!isValidTask(currentTitle, currentDescription)) {
-            return
+        if (jokeSetup == emptyValue){
+            jokesData = JokesData(
+                joke = delivery,
+                type = categories.value.toString()
+            )
+        }else{
+            jokesData = JokesData(
+                setup = jokeSetup,
+                delivery = delivery,
+                type = categories.value.toString()
+            )
         }
 
-        if (currentTaskID == noTaskID) {
-            createTask(task)
-        } else {
-            updateTask(task)
-        }
+        saveJokes(jokesData)
 
-    }*/
+    }
+
+    private fun saveJokes(jokesData: JokesData){
+        viewModelScope.launch{
+            repository.saveJokes(jokesData)
+        }
+    }
 }
 
 
